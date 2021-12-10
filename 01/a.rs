ac7b1326d8d1441fc473b22ca1df9d0fc98b2d07
Jasper Krauter <jasper.krauter@uni-konstanz.de>
2021-12-10 18:11:05 +0100

    day 1, but with windows

--- 01/a.rs
+++ 01/a.rs
@@ -2,16 +2,12 @@ use std::io;
 use std::io::BufRead;
 
 fn main() {
+    let input : Vec<_> = io::stdin().lock().lines().map(|l| l.unwrap().parse::<u32>().unwrap()).collect();
     let mut counter = 0;
-    let mut last = 0;
-    let mut first = true;
-    for line in io::stdin().lock().lines() {
-        let num = line.unwrap().parse::<u32>().unwrap();
-        if !first && last < num {
-            counter = counter + 1;
+    for window in input.windows(2) {
+        if window[0] < window[1] {
+            counter += 1;
         }
-        last = num;
-        first = false;
     }
     println!("{}", counter);
 }
--- 01/b.rs
+++ 01/b.rs
@@ -2,19 +2,12 @@ use std::io;
 use std::io::BufRead;
 
 fn main() {
+    let input : Vec<_> = io::stdin().lock().lines().map(|l| l.unwrap().parse::<u32>().unwrap()).collect();
     let mut counter = 0;
-    let mut nums: [u32; 3] = [0; 3];
-    let mut i = 0;
-    let mut sum = 0;
-    for line in io::stdin().lock().lines() {
-        let new = line.unwrap().parse::<u32>().unwrap();
-        nums[i % 3] = new;
-        let new_sum = nums.iter().sum();
-        if i >= 3 && new_sum > sum {
-            counter = counter + 1;
+    for window in input.windows(4) {
+        if window[0] + window[1] + window[2] < window[1] + window[2] + window[3] {
+            counter += 1;
         }
-        sum = new_sum;
-        i = i + 1;
     }
     println!("{}", counter);
 }
