d52ced77b732f2058719de7013361c749e64343e
Jasper Krauter <jasper.krauter@uni-konstanz.de>
2021-12-12 23:46:35 +0100

    day 12, but more elegant code

--- 12/a.rs
+++ 12/a.rs
@@ -3,38 +3,31 @@ use std::collections::HashSet;
 use std::io;
 use std::io::BufRead;
 
-fn count_paths(graph : &HashMap::<String, Vec<String>>, mut visited : HashSet<String>, v : String) -> u32 {
+fn count_paths<'a>(graph : &HashMap::<&'a str, Vec<&'a str>>, mut visited : HashSet<&'a str>, v : &'a str) -> u32 {
     if v == "end" {
         return 1;
     }
-    if v.clone().chars().all(|c| matches!(c, 'a'..='z')) && visited.contains(&v) {
+    if v.to_lowercase() == v && visited.contains(&v) {
         return 0;
     }
-    visited.insert(v.clone());
+    visited.insert(v);
     let mut sum = 0;
     for w in &graph[&v] {
-        sum += count_paths(graph, visited.clone(), w.to_string());
+        sum += count_paths(graph, visited.clone(), w);
     }
     return sum;
 }
 
 fn main() {
     let lines : Vec<_> = io::stdin().lock().lines().map(|l| l.unwrap()).collect();
-    let mut graph = HashMap::<String, Vec<String>>::new();
-    for line in lines.iter().map(|l| l.split("-").map(|s| s.to_string()).collect::<Vec<String>>()) {
-        if graph.contains_key(&line[0]) {
-            graph.get_mut(&line[0]).unwrap().push(line[1].clone());
-        } else {
-            graph.insert(line[0].clone(), vec![line[1].clone()]);
-        }
-        if graph.contains_key(&line[1]) {
-            graph.get_mut(&line[1]).unwrap().push(line[0].clone());
-        } else {
-            graph.insert(line[1].clone(), vec![line[0].clone()]);
-        }
+    let mut graph = HashMap::<&str, Vec<&str>>::new();
+    for mut line in lines.iter().map(|l| l.split("-")) {
+        let tail = line.next().unwrap();
+        let head = line.next().unwrap();
+        graph.entry(tail).or_default().push(head);
+        graph.entry(head).or_default().push(tail);
     }
 
-    let visited = HashSet::<String>::new();
-    let kek = count_paths(&graph, visited, "start".to_string());
-    println!("{}", kek);
+    let visited = HashSet::<&str>::new();
+    println!("{}", count_paths(&graph, visited, "start"));
 }
